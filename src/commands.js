d2fb7c5e7c497af7ebaf2eb9439f69f3589cb916
Glitch (chalerm-bot) <none>
2024-02-02 06:46:21 +0000

    👚🛰 Checkpoint ./src/commands.js:64647692/3911

--- src/commands.js
+++ src/commands.js
@@ -1,14 +1,14 @@
-import 'dotenv/config';
-import { installGlobalCommands } from './utils.js';
+import "dotenv/config";
+import { installGlobalCommands } from "./utils.js";
 
 const CONVERT_URL_COMMAND = {
-  name: 'url',
-  description: 'Convert a URL to a manybaht link',
+  name: "url",
+  description: "Convert a URL to a manybaht link",
   options: [
     {
       type: 3,
-      name: 'url',
-      description: 'URL to convert',
+      name: "url",
+      description: "URL to convert",
       required: true,
     },
   ],
@@ -16,24 +16,38 @@ const CONVERT_URL_COMMAND = {
 };
 
 const MY_FAVORITE_COMMAND = {
-  name: 'fav',
-  description: 'List of your favorite songs',
+  name: "fav",
+  description: "List of your favorite songs",
   type: 1,
-}
-
+};
 
 const ADD_FAV_COMMAND = {
-  name: 'add',
-  description: 'add favourite your favorite songs',
-  type: 1,
-}
+  name: "add",
+  description: "add favourite your favorite songs",
+  options: [
+    {
+      type: 3,
+      name: "url",
+    },
+    {
+      type: 3,
+      name: "title",
+    },
+  ],
+  type: 2,
+};
 
 const DEL_FAV_COMMAND = {
-   name: 'del',
-  description: 'add favourite your favorite songs',
+  name: "del",
+  description: "add favourite your favorite songs",
   type: 1,
-}
+};
 
-const ALL_COMMANDS = [CONVERT_URL_COMMAND, MY_FAVORITE_COMMAND, MY_LIST_COMMAND];
+const ALL_COMMANDS = [
+  CONVERT_URL_COMMAND,
+  MY_FAVORITE_COMMAND,
+  ADD_FAV_COMMAND,
+  DEL_FAV_COMMAND,
+];
 
 installGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
