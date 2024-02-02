8dc8512bad5d69ca103fe7cdbf64c67513a7c8d6
Pitsanu L. <30042217+pitsanujiw@users.noreply.github.com>
2024-02-02 17:16:59 +0700

    fix: fix delete

--- src/app.js
+++ src/app.js
@@ -172,7 +172,7 @@ app.post('/api/interactions', async (req, res) => {
 
             const song = dataStore.favorites[userId]?.songs?.find((song) => song.id === id.value);
 
-            const index = dataStore.favorites[userId]?.songs?.findIndex((song) => song.id !== id.value);
+            const index = dataStore.favorites[userId]?.songs?.findIndex((song) => song.id === id.value);
 
             dataStore.favorites[userId].songs.splice(index, 1);
 
