bf4d5143c93aedf403e9e48b8f4c4b979f64f86e
Pitsanu L. <30042217+pitsanujiw@users.noreply.github.com>
2024-02-02 15:55:30 +0700

    fix: update get path name

--- src/app.js
+++ src/app.js
@@ -93,11 +93,11 @@ app.post('/api/interactions', async (req, res) => {
 
         case InteractType.ADD:
           try {
-            console.log(import.meta.url);
-            const json = await readFile(`${__dirname}/src/data/stores.json`);
-            const favorites = JSON.parse(json);
+            console.log(new URL(import.meta.url).pathname);
+            // const json = await readFile(`${__dirname}/src/data/stores.json`);
+            // const favorites = JSON.parse(json);
 
-            console.log(favorites, data);
+            console.log(data);
 
             return res.send({
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
